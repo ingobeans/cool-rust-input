6f6cf9567dac1bfdb8d2c1355f83bdde6238cc9b
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-11-09 12:50:06 +0100

    enable example code

--- src/main.rs
+++ src/main.rs
@@ -14,35 +14,34 @@ impl CustomInput for CoolCustomInput {
         return false;
     }
     fn after_draw_text(&mut self, terminal_size: (u16, u16)) {
-        //println!("\x1b[1;0H    Welcome to my cool text editor. Here you can write cool stuff!");
-        //println!("\x1b[2;0H    Rules: ");
-        //println!("\x1b[3;0H        None!!");
-        //println!("\x1b[4;0H    {}", String::from("_").repeat((terminal_size.0 as usize) - 10));
+        println!("\x1b[1;0H    Welcome to my cool text editor. Here you can write cool stuff!");
+        println!("\x1b[2;0H    Rules: ");
+        println!("\x1b[3;0H        None!!");
+        println!("\x1b[4;0H    {}", String::from("_").repeat((terminal_size.0 as usize) - 10));
 
-        //set_terminal_line(
-        //    "Welcome to my cool text editor. Here you can write cool stuff!",
-        //    5,
-        //    0
-        //).unwrap();
-        //set_terminal_line("Rules:", 5, 1).unwrap();
-        //set_terminal_line("None!!", 10, 2).unwrap();
-        //set_terminal_line(
-        //    &String::from("_").repeat((terminal_size.0 as usize) - 10),
-        //    5,
-        //    3
-        //).unwrap();
+        set_terminal_line(
+            "Welcome to my cool text editor. Here you can write cool stuff!",
+            5,
+            0
+        ).unwrap();
+        set_terminal_line("Rules:", 5, 1).unwrap();
+        set_terminal_line("None!!", 10, 2).unwrap();
+        set_terminal_line(
+            &String::from("_").repeat((terminal_size.0 as usize) - 10),
+            5,
+            3
+        ).unwrap();
     }
     fn get_offset(&mut self, terminal_size: (u16, u16)) -> (u16, u16) {
-        (0, 0)
+        (5, 5)
     }
     fn get_size(&mut self, terminal_size: (u16, u16)) -> (u16, u16) {
-        //(terminal_size.0 - 10, 5)
-        (terminal_size.0, terminal_size.1)
+        (terminal_size.0 - 10, 5)
     }
 }
 
 fn main() {
-    //let mut cool_input = CoolInput::new(CoolCustomInput);
-    let mut cool_input = CoolInput::new(DefaultInput);
+    let mut cool_input = CoolInput::new(CoolCustomInput);
+    //let mut cool_input = CoolInput::new(DefaultInput);
     cool_input.listen().unwrap();
 }
